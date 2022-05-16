e0d01cd974e821d5345b864b38519fc870434dee
Nikita Shchetko <corrosion.by@gmail.com>
2022-05-16 15:59:40 +0400

    changed folder structure + basic updates;

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 function doGet(request) {
-  var html = HtmlService.createTemplateFromFile('index');
-  var evaluated = html.evaluate();
-  evaluated.addMetaTag('viewport', 'width=device-width, initial-scale=1');
-  return evaluated
+    var html = HtmlService.createTemplateFromFile('index');
+    var evaluated = html.evaluate();
+    evaluated.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+    return evaluated
 }
 
 function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename)
+return HtmlService.createHtmlOutputFromFile(filename)
     .getContent();
 }
